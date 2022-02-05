6331cd34c0ef7aa359734b6c773bba84f4ac3313
Veronica Cesari Carson <verocesari@gmail.com>
2022-02-05 16:34:05 -0300

    Formulario completo

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Formulario= () => {
+const Formulario= ({guardarBusquedaLetra}) => {
+
+    const [busqueda, guardarBusqueda] = useState({
+        artista: '',
+        cancion: ''
+    });
+
+    const [error, guardarError] = useState(false);
+
+    const {artista, cancion} = busqueda;
+
+    //función a cada input para leer su contenido
+
+    const actualizarState = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value    
+        })
+    }
+
+    //consultar las apis
+
+    const buscarInformacion = e => {
+        e.preventDefault();
+
+        if(artista.trim() === '' || cancion.trim() === ''){
+                guardarError(true);
+                return;
+        }
+        guardarError(false);
+        //Si está bien pasa al componente principal 
+        guardarBusquedaLetra(busqueda);
+    }
+
     return(
        <div className='bg-info'>
+           {error ? <p className='alert alert-danger text-center p-2'>Todos los campos son obligatorios</p> : null}
            <div className='container'>
                <div className='row'>
+                   
                    <form
+                    onSubmit={buscarInformacion}
                      className='col card text-white bg-transparent mb-5 pt-5 pb-2'
                    >
                          <fieldset>
@@ -19,6 +55,8 @@ const Formulario= () => {
                       className="form-control"
                       name="artista"
                       placeholder="Nombre Artista" 
+                      onChange={actualizarState}
+                      value={artista}
                     />
                   </div>
                 </div>
@@ -30,7 +68,8 @@ const Formulario= () => {
                       className="form-control"
                       name="cancion"
                       placeholder="Nombre Canción"
-                     
+                      onChange={actualizarState}
+                      value={cancion}
                     />
                   </div>
                 </div>
